b9807480b07a25a5895c283fa825a2c618d1d879
Feng Yao <46351482+feng-y16@users.noreply.github.com>
2019-04-12 12:12:53 +0800

    Add files via upload

---
+++ specific_problem_3.m
@@ -0,0 +1,11 @@
+function [c_obj,A1,b1,v1,v2] = specific_problem_3(F,Fi,c,ci,pi_,yi,u)
+c_obj=[pi_(1)*Fi(1);pi_(2)*Fi(2);pi_(3)*Fi(3)];
+A1=[-Fi(1)/(F+Fi(1)),0,0;
+    -Fi(1)*yi(1)/(F+Fi(1)+Fi(2)),-Fi(2)/(F+Fi(1)+Fi(2)),0;
+    -Fi(1)*yi(1)*yi(2)/(F+Fi(1)+Fi(2)+Fi(3)),-Fi(2)*yi(2)/(F+Fi(1)+Fi(2)+Fi(3)),-Fi(3)/(F+Fi(1)+Fi(2)+Fi(3))];
+b1=[u-((F*c+Fi(1)*ci(1))/(F+Fi(1)));
+    u-((F*c*yi(1)+Fi(1)*ci(1)*yi(1)+Fi(2)*ci(2))/(F+Fi(1)+Fi(2)));
+    u-((F*c*yi(1)*yi(2)+Fi(1)*ci(1)*yi(1)*yi(2)+Fi(2)*ci(2)*yi(2)+Fi(3)*ci(3))/(F+Fi(1)+Fi(2)+Fi(3)))];
+v1=[0;0;0];
+v2=[ci(1);ci(2);ci(3)];
+end
